9f15d5d1c7ba94d5d6a65ee88f50172c2d5bd536
Sitesh <siteshpro@gmail.com>
2022-01-26 17:23:43 +0530

    404 page contents added

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,12 +10,16 @@ export default function FourZeroFour() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          1729..... This page doesn't exist 🤯.
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-2">I know it sounds crazy.</p>
+        <p className="mb-8">
+          But <b>Don't panic</b>, please check if there is a <i>typo in the URL</i> or Start from
+          Home.
+        </p>
         <Link href="/">
           <button className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue hover:bg-blue-700 dark:hover:bg-blue-500">
-            Back to homepage
+            Return Home
           </button>
         </Link>
       </div>
