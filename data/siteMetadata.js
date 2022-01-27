999da2f59aff817dec99d737d25e358b5c6337ce
Sitesh <siteshpro@gmail.com>
2022-01-27 15:28:13 +0530

    Changed sitedescr. fter credit pding & +socialCard

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,9 @@
 const siteMetadata = {
-  title: 'Sitesh',
+  title: 'Sitesh Kumar Sahoo',
   author: 'Sitesh Kumar Sahoo',
   headerTitle: 'Sitesh',
-  description: 'Get an unfair advantage',
+  description:
+    'Writing essays to make technology, Web 3.0 and complex concepts heard in the simplest way',
   language: 'en-us',
   theme: 'dark', // system, dark or light
   siteUrl: 'https://sitesh.me',
@@ -19,7 +20,7 @@ const siteMetadata = {
   locale: 'en-US',
   analytics: {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: '', // e.g. sitesh.io.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
