a732b3cfac5f9e36e5b7c01187f587a02d07518a
Sitesh <siteshpro@gmail.com>
2022-01-28 13:17:45 +0530

    Fixed MobilePostLayout

--- components/PostContainer .tsx
+++ components/PostContainer .tsx
@@ -6,6 +6,6 @@ interface Props {
 
 export default function PostContainer({ children }: Props) {
   return (
-    <div className="max-w-3xl px-4 mx-auto sm:px-4 xl:max-w-3xl xl:px-6 xl:px-14">{children}</div>
+    <div className="max-w-3xl px-2 mx-auto sm:px-4 xl:max-w-3xl xl:px-6 xl:px-14">{children}</div>
   )
 }
