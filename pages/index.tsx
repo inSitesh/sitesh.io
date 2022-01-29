f3be39faa936bd283c033541a90fc80d4dc71bf7
Sitesh <siteshpro@gmail.com>
2022-01-29 12:12:18 +0530

    Removed tags from layout and Resized the cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <div className="flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
+      <div className="flex flex-col justify-center items-start max-w-3xl border-gray-200 dark:border-gray-700 mx-auto pb-12">
         <div className="flex flex-col-reverse sm:flex-row items-start">
           <div className="flex flex-col pr-8">
             <h1 className="font-bold tracking-wide text-3xl md:text-4xl md:pb-1 mb-1 text-black dark:text-white pt-6  ">
@@ -33,12 +33,12 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
               </span>{' '}
               #Ship
             </h2>
-            <p className="font-bold text-transparent text-xl text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 via-blue-500 to-emerald-700 dark:text-transparent text-xl bg-clip-text bg-gradient-to-r from-blue-700 via-blue-500 to-emerald-500 mb-16">
+            <p className="font-bold text-transparent text-xl text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 via-blue-500 to-emerald-700 dark:text-transparent text-xl bg-clip-text bg-gradient-to-r from-blue-700 via-blue-500 to-emerald-500 mb-14">
               Writing essays to make technology, Web 3.0 and complex concepts heard in the simplest
               way.
             </p>
             <div className="flex flex-col divide-y-grey-600 space-y-5 md:space-y-5"></div>
-            <h3 className="text-xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 sm:text-2xl md:text-2xl md:leading-12 underline decoration-blue-500 underline-offset-8 ">
+            <h3 className="text-xl font-semibold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 sm:text-2xl md:text-2xl md:leading-12 underline decoration-blue-500 underline-offset-8  pb-2">
               Recent posts
             </h3>
             {/* <p className="text leading-7 divide-grey-200 text-gray-500 dark:text-gray-400 ">
@@ -52,19 +52,19 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
             const { slug, date, title, summary, tags } = frontMatter
             return (
-              <li key={slug} className="py-7">
+              <li key={slug} className="py-4">
                 <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+                  <div className="space-y-1 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
                     <dl>
                       <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                      <dd className="text-sm font-medium leading-4 text-gray-500 dark:text-gray-500">
                         <time dateTime={date}>{formatDate(date)}</time>
                       </dd>
                     </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
+                    <div className="space-y-1 xl:col-span-3">
+                      <div className="space-y-0">
                         <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                          <h2 className="text-xl font-bold leading-8 tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
@@ -72,11 +72,11 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
                               {title}
                             </Link>
                           </h2>
-                          <div className="flex flex-wrap">
+                          {/* <div className="flex flex-wrap">
                             {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
                             ))}
-                          </div>
+                          </div> */}
                         </div>
                         <div className="prose text-gray-500 max-w-none dark:text-gray-400">
                           {summary}
@@ -85,7 +85,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
                       <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          className="text-primary-400 hover:text-primary-600 dark:hover:text-primary-300"
                           aria-label={`Read "${title}"`}
                         >
                           Read more &rarr;
@@ -100,10 +100,10 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         </ul>
       </div>
       {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
+        <div className="flex justify-end text-base font-semibold leading-6 pb-4">
           <Link
             href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            className="text-primary-600 hover:text-primary-500 dark:hover:text-primary-400"
             aria-label="all posts"
           >
             All Posts &rarr;
